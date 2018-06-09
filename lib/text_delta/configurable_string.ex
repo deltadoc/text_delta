612f29aaf1628a6b44c39a9d8327d349207938c4
everzet <ever.zet@gmail.com>
2018-06-09 11:54:05 +0100

    Fix linting errors

--- lib/text_delta/configurable_string.ex
+++ lib/text_delta/configurable_string.ex
@@ -3,12 +3,10 @@ defmodule TextDelta.ConfigurableString do
   String configurable to support unicode or not.
   """
 
-  @support_unicode Application.get_env(:text_delta, :support_unicode, true)
-
   @doc "Calculates the length of a given string"
   @spec length(String.t()) :: non_neg_integer
   def length(str) do
-    if @support_unicode do
+    if Application.get_env(:text_delta, :support_unicode, true) do
       String.length(str)
     else
       byte_size(str)
@@ -18,7 +16,7 @@ defmodule TextDelta.ConfigurableString do
   @doc "Splits given string at the index"
   @spec split_at(String.t(), non_neg_integer) :: {String.t(), String.t()}
   def split_at(str, idx) do
-    if @support_unicode do
+    if Application.get_env(:text_delta, :support_unicode, true) do
       String.split_at(str, idx)
     else
       {binary_part(str, 0, idx), binary_part(str, idx, byte_size(str) - idx)}
